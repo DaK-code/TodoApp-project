afe80f7080c9255c2866e1bf8454a44b5e5c240b
DaK-code <dariusgavi124@gmail.com>
2025-10-30 10:06:19 +0000

    add input tag but do not work

--- todoApp/src/App.jsx
+++ todoApp/src/App.jsx
@@ -2,11 +2,13 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-
+  const [text, setText] = useState("")
 
   return (
     <div>
-      
+      <div>
+        <input type="text" value = {text} />
+      </div>
     </div>
   )
 }
