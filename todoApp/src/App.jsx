494a98fe0c184af4f64592d6a9aca2f3afa94f74
DaK-code <dariusgavi124@gmail.com>
2025-10-30 10:58:05 +0000

    add new feature to create a task list

--- todoApp/src/App.jsx
+++ todoApp/src/App.jsx
@@ -3,17 +3,26 @@ import './App.css'
 
 function App() {
   const [text, setText] = useState("")
+  const [tasks, setTasks] = useState([])
 
   const handleChange = (e)=>{
     setText(e.target.value)
+  }
+
+  const handleTasks = (text) =>{
+    const newTask = [...tasks, text]
+    setTasks(newTask)
+    setText("")
   }
 
   return (
     <div>
       <div>
         <input type="text" value = {text} onChange={handleChange} />
-        <button>Add New Task</button>
+        <button onClick={()=>handleTasks(text)}>Add New Task</button>
       </div>
+      {tasks.map(task => <p>{task}</p>)}
+
     </div>
   )
 }
